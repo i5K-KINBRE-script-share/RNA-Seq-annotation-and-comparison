d80b83ec0737beb34f836fd8d15dcae25d694211
kstatebioinfo <kstate.bioinformatics@gmail.com>
2013-09-09 00:35:31 -0500

    now prints tab separated columns

--- KSU_bioinfo_lab/count_reads_aligned_to_de_novo_transcriptome.pl
+++ KSU_bioinfo_lab/count_reads_aligned_to_de_novo_transcriptome.pl
@@ -5,14 +5,20 @@ use Data::Dumper;
 use IO::File;
 #  count_reads_aligned_to_de_novo_transcriptome.pl
 #  USAGE: perl count_reads_aligned_to_de_novo_transcriptome.pl [filename/s]
-# This script takes sam files (one per biological or technical replicate) and outputs a hash where the keys are the name of the contig and the values are an array with one element per sample (in the same order as you listed your sam files)
+# This script takes sam files (one per biological or technical replicate) and outputs tab separated list where the first column is the name of the contig and the values are the read counts per sample (in the same order as you listed your sam files)
 #
 #  Created by jennifer shelton on 6/23/13.
 ##################  define variables #################################################
 my $outfile="counts.txt";
-my (@temp);
+my (@temp,@default);
 my (%hash);
+my $contig;
 my $i=0;
+for (0..$#ARGV)
+{
+    push(@default,'0');
+}
+
 open (COUNT_FILES, ">$outfile");
 foreach my $f (@ARGV)
 {
@@ -25,7 +31,7 @@ foreach my $f (@ARGV)
 	        print "($temp[2])\n";
 	        if (! $hash{$temp[2]})
 	        {
-	        	$hash{$temp[2]} =[];
+	        	$hash{$temp[2]} =[@default];
 	        }
 	       	$hash{$temp[2]}->[$i]++;
 	    }
@@ -34,5 +40,15 @@ foreach my $f (@ARGV)
 	$file->close;
 }
 
-print COUNT_FILES Dumper( \%hash );	        
+print COUNT_FILES Dumper( \%hash );
+foreach $contig ( keys %hash )
+{
+    print COUNT_FILES "$contig\t";
+    foreach $i ( 0 .. $#{ $hash{$contig} } )
+    {
+        print COUNT_FILES "$hash{$contig}[$i]\t";
+    }
+    print COUNT_FILES "\n";
+}
+
 	
