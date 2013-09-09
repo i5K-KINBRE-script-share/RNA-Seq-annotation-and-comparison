3f578536e69606e09da19b2d04365d6f02f11794
kstatebioinfo <kstate.bioinformatics@gmail.com>
2013-09-09 13:46:12 -0500

    Update count_reads_aligned_to_de_novo_transcriptome.pl
    # This script takes sam files (one per biological or technical replicate) and outputs tab separated list where the first column is the name of the contig and the values are the read counts per sample (in the same order as you listed your sam files)
    # The script will print out sample ids. replace the example sample ids in line 17 with your own before running. This step is optional. Uncomment line 16 to 17 and lines 47 to 57 if you want these printed.

--- KSU_bioinfo_lab/count_reads_aligned_to_de_novo_transcriptome.pl
+++ KSU_bioinfo_lab/count_reads_aligned_to_de_novo_transcriptome.pl
@@ -6,21 +6,24 @@ use IO::File;
 #  count_reads_aligned_to_de_novo_transcriptome.pl
 #  USAGE: perl count_reads_aligned_to_de_novo_transcriptome.pl [filename/s]
 # This script takes sam files (one per biological or technical replicate) and outputs tab separated list where the first column is the name of the contig and the values are the read counts per sample (in the same order as you listed your sam files)
+# The script will print out sample ids. replace the example sample ids in line 17 with yourown before running. This step is optional. Uncomment line 16 to 17 and lines 47 to 57 if you want these printed.
 #
 #  Created by jennifer shelton on 6/23/13.
 ##################  define variables #################################################
-my $outfile="counts.txt";
+my $outfile="counts.txt"; # the name of the output file can be changed here
 my (@temp,@default);
 my (%hash);
-my $contig;
+#my $j;
+#my (@samples)=qw/sample1 sample2 sample3/; #replace with your sample ids in the order that you list your sam files
+my ($contig,$k);
 my $i=0;
-for (0..$#ARGV)
+for (0..$#ARGV) ## populate default values for each contig
 {
     push(@default,'0');
 }
 
-open (COUNT_FILES, ">$outfile");
-foreach my $f (@ARGV)
+open (COUNT_FILES, ">$outfile"); 
+foreach my $f (@ARGV) ## make a hash of raw counts
 {
 	my $file = IO::File->new($f);
 	while (<$file>)
@@ -40,15 +43,32 @@ foreach my $f (@ARGV)
 	$file->close;
 }
 
-print COUNT_FILES Dumper( \%hash );
-foreach $contig ( keys %hash )
+
+# foreach $j (0 .. $#samples) ## optional printing a samplids, comment out lines 45 to 55 if you do not want these
+# {
+# 	if ($j < $#samples)
+# 	{
+# 		print COUNT_FILES "$samples[$j]\t";
+# 	}
+# 	else
+# 	{
+# 		print COUNT_FILES "$samples[$j]\n";
+# 	}
+# }
+foreach $contig ( keys %hash ) ## print out table of raw counts
 {
     print COUNT_FILES "$contig\t";
-    foreach $i ( 0 .. $#{ $hash{$contig} } )
+    foreach $k ( 0 .. $#{ $hash{$contig} } )
     {
-        print COUNT_FILES "$hash{$contig}[$i]\t";
-    }
-    print COUNT_FILES "\n";
+    	if ($k < $#{ $hash{$contig} })
+    	{
+        	print COUNT_FILES "$hash{$contig}[$k]\t";
+    	}
+        else
+        {
+        	print COUNT_FILES "$hash{$contig}[$k]\n";
+        }
+	}
 }
 
 	
