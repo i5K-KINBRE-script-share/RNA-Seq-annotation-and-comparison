e7896f053ddc09d4216e1dcf2f23876b1190267c
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2013-12-30 16:21:02 -0600

    Update blastx.pl

--- KSU_bioinfo_lab/blastx.pl
+++ KSU_bioinfo_lab/blastx.pl
@@ -1,6 +1,6 @@
 #!/bin/perl
 ##################################################################################
-#   
+#
 # USAGE: perl blastx.pl [FASTA filename]
 # Script output fasta records split into files  of 1000 or less sequences in a directory called split. It also creates blastx bash scripts and qsub commands.
 #  Created by jennifer shelton 12/30/13
@@ -18,7 +18,7 @@ use File::Basename; # enable maipulating of the full path
 ##################################################################################
 my $file_count=0;
 my $seq_count=0;
-my $max_seqs=1000;
+my $max_seqs=100;
 my $outfilename;
 ##################################################################################
 ##############              open fasta get fullpath             ##################
@@ -36,13 +36,14 @@ open QSUBS, '>', $subs or die "Couldn't open $subs !\n";
 
 `mkdir ${directories}split`;
 `mkdir ${directories}jobs`;
+`mkdir ${directories}blasts`;
 while(<OLD_FASTA>)
 {
 	unless (${file_count} == 0) ## skip first record (it is a blank line)
 	{
-##################################################################################
-##############    open new file create jobs and qsub commands   ##################
-##################################################################################
+        ##################################################################################
+        ##############    open new file create jobs and qsub commands   ##################
+        ##################################################################################
 		if ($seq_count==0)
 		{
 			$outfilename="${directories}split/${filename}_${file_count}.fasta";
@@ -50,30 +51,30 @@ while(<OLD_FASTA>)
 			my $bashs="${directories}jobs/${filename}_${file_count}.sh";
 			open SH, '>', $bashs  or die "Couldn't open $bashs !\n";
 			print SH '#!/bin/bash';
-			print SH "\n/homes/bioinfo/bioinfo_software/ncbi-blast-2.2.28+/bin/blastx -query ${outfilename} -db  /homes/bioinfo/bioinfo_software/blastdb/nr -evalue 1e-05 -out ${directories}split/${filename}_${file_count}.xml -outfmt 5 -max_target_seqs 1 -num_threads 16\n";
-			print QSUBS "qsub -l mem=2G,h_rt=672 -pe single 16 ${bashs}\n";
+			print SH "\n/homes/bioinfo/bioinfo_software/ncbi-blast-2.2.28+/bin/blastx -query ${outfilename} -db  /homes/bioinfo/bioinfo_software/blastdb/nr -evalue 1e-05 -out ${directories}blasts/${filename}_${file_count}.xml -outfmt 5 -max_target_seqs 1 -num_threads 16\n";
+			print QSUBS "qsub -l mem=2G,h_rt=3:00:00 -pe single 16 ${bashs}\n";
 			++$file_count;
 		}
-##################################################################################
-##############            populate fasta file                   ##################
-##################################################################################
-
+        ##################################################################################
+        ##############            populate fasta file                   ##################
+        ##################################################################################
+        
 		if ($seq_count!=0)
 		{
             print OUTFILE '>';
     		print OUTFILE "$_";
     	}
-##################################################################################
-##############                 reset counter                    ##################
-##################################################################################
-		if ($seq_count==1000)
+        ##################################################################################
+        ##############                 reset counter                    ##################
+        ##################################################################################
+		if ($seq_count==$max_seqs)
 		{
 			$seq_count=-1; ## reset counter
 			close (OUTFILE);
-		}	
-	++$seq_count;
+		}
+        ++$seq_count;
     }
-    else 
+    else
     {
     	${file_count}=1; ## skip first record (it is a blank line)
     }
