a2b853af8462383d7a1caa8f55f1b21db0f71fd5
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2013-12-31 06:00:39 -0600

    Update blastx.pl

--- KSU_bioinfo_lab/blastx.pl
+++ KSU_bioinfo_lab/blastx.pl
@@ -2,7 +2,7 @@
 ##################################################################################
 #
 # USAGE: perl blastx.pl [FASTA filename]
-# Script output fasta records split into files  of ~100 or less sequences in a directory called split. It also creates blastx bash scripts and qsub commands.
+# Script output fasta records split into files  of 100 or less sequences in a directory called split. It also creates blastx bash scripts and qsub commands.
 #  Created by jennifer shelton 12/30/13
 #
 ##################################################################################
@@ -16,7 +16,7 @@ use File::Basename; # enable maipulating of the full path
 ##################################################################################
 ##############             Initialize variables                 ##################
 ##################################################################################
-my $file_count=0;
+my $file_count=1;
 my $seq_count=0;
 my $max_seqs=100;
 my $outfilename;
@@ -39,12 +39,12 @@ open QSUBS, '>', $subs or die "Couldn't open $subs !\n";
 `mkdir ${directories}blasts`;
 while(<OLD_FASTA>)
 {
-	unless (${file_count} == 0) ## skip first record (it is a blank line)
+	unless (${seq_count} == 0) ## skip first record (it is a blank line)
 	{
         ##################################################################################
         ##############    open new file create jobs and qsub commands   ##################
         ##################################################################################
-		if ($seq_count==0)
+		if ($seq_count==1)
 		{
 			$outfilename="${directories}split/${filename}_${file_count}.fasta";
 			open OUTFILE, '>', $outfilename  or die "Couldn't open $outfilename !\n";
@@ -52,31 +52,25 @@ while(<OLD_FASTA>)
 			open SH, '>', $bashs  or die "Couldn't open $bashs !\n";
 			print SH '#!/bin/bash';
 			print SH "\n/homes/bioinfo/bioinfo_software/ncbi-blast-2.2.28+/bin/blastx -query ${outfilename} -db  /homes/bioinfo/bioinfo_software/blastdb/nr -evalue 1e-05 -out ${directories}blasts/${filename}_${file_count}.xml -outfmt 5 -max_target_seqs 1 -num_threads 16\n";
-			print QSUBS "qsub -l mem=2G,h_rt=3:00:00 -pe single 16 ${bashs}\n";
+			print QSUBS "qsub -l mem=1G,h_rt=4:00:00 -pe single 16 -m abe -M jennifer.shelton\@gmail.com ${bashs}\n";
 			++$file_count;
 		}
         ##################################################################################
         ##############            populate fasta file                   ##################
         ##################################################################################
-        
-		if ($seq_count!=0)
-		{
-            print OUTFILE '>';
-    		print OUTFILE "$_";
-    	}
+		print OUTFILE '>';
+		s/>//g;
+		print OUTFILE "$_";
         ##################################################################################
         ##############                 reset counter                    ##################
         ##################################################################################
 		if ($seq_count==$max_seqs)
 		{
-			$seq_count=-1; ## reset counter
+			$seq_count=0; ## reset counter
 			close (OUTFILE);
 		}
-        ++$seq_count;
+        
     }
-    else
-    {
-    	${file_count}=1; ## skip first record (it is a blank line)
-    }
+    ++$seq_count;
 }
 close (OLD_FASTA);
